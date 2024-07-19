746711e90d1b3cd91eb5fe5f8e0b2154ddcc0410
lissbest7 <vahidsrifzad@gmail.com>
2024-07-19 19:42:33 +0400

    cart

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,17 @@
 import { LuShoppingCart, LuHeart, LuUserCircle } from "react-icons/lu";
 import "./Navbar.css";
+import { useState } from "react";
 import logo from "../img/logo.png";
+import shoe from "../img/shoe.png";
+import shoe1 from "../img/shoe1.png";
 function Navbar() {
+  const [cartOpen, setCartOpen] = useState(false);
+
+  
+  const toggleCart = () => {
+    setCartOpen(!cartOpen);
+  };
+
   return (
     <header className="navbar">
       <div className="navbar-nav container">
@@ -15,9 +25,9 @@ function Navbar() {
           </div>
         </div>
         <div className="navbar-right">
-          <li>
+          <li onClick={toggleCart}>
             <LuShoppingCart />
-            <p>13,69 $</p>
+            <p>246,54 $</p>
           </li>
           <li>
             <LuHeart />
@@ -29,6 +39,36 @@ function Navbar() {
           </li>
         </div>
       </div>
+      {cartOpen && (
+        <div className="cart-menu">
+          <h2>Shopping cart</h2>
+          <div className="cart-products">
+            <div className="cart-product">
+              <div className="cart-product__image">
+                <img src={shoe1} alt="Nike Air Max 270" />
+              </div>
+              <div className="cart-product__text">
+                <h3>Nike Air Max 270 Men's Sneakers</h3>
+                <p>149,07 $</p>
+              </div>
+            </div>
+            <div className="cart-product">
+              <div className="cart-product__image">
+                <img src={shoe} alt="Nike Blazer Mid Suede" />
+              </div>
+              <div className="cart-product__text">
+                <h3>Nike Blazer Mid Suede Men's Sneakers</h3>
+                <p>97,47 $</p>
+              </div>
+            </div>
+          </div>
+          <div className="cart-summary">
+            <p>Total: 246,54 $</p>
+            <p>Tax 5%: 12,32 $</p>
+          </div>
+          <button className="summary-button">Submit</button>
+        </div>
+      )}
     </header>
   );
 }
