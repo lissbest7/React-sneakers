afda2ba08e643f4503d8f8630760479d918b60cd
lissbest7 <vahidsrifzad@gmail.com>
2024-07-19 18:54:40 +0400

    search

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,5 +1,6 @@
 import ProductCard from "../ProductCard/ProductCard";
 import "./Cards.css";
+import { LuSearch } from "react-icons/lu";
 import shoe from "../img/shoe.png";
 import shoe1 from "../img/shoe1.png";
 import shoe2 from "../img/shoe2.png";
@@ -12,7 +13,8 @@ import shoe8 from "../img/shoe8.png";
 import shoe9 from "../img/shoe9.png";
 import shoe10 from "../img/shoe10.png";
 import shoe11 from "../img/shoe11.png";
-let products = [
+import { useState } from "react";
+let productsData = [
   {
     productPrice: "148,1 $",
     productTitle: "Nike Blazer Mid Suede Men's Sneakers",
@@ -34,7 +36,7 @@ let products = [
     img: shoe3,
   },
   {
-    productPrice: "173,06",
+    productPrice: "173,06 $",
     productTitle: "Men's Under Armor Curry 8 Sneakers",
     img: shoe4,
   },
@@ -54,7 +56,7 @@ let products = [
     img: shoe7,
   },
   {
-    productPrice: "159,39",
+    productPrice: "159,39 $",
     productTitle: "Nike Lebron XVIII Low Men's Sneakers",
     img: shoe8,
   },
@@ -75,21 +77,42 @@ let products = [
   },
 ];
 
+
 function Cards() {
+  const [products, setProducts] = useState(productsData);
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredProducts = products.filter((product) =>
+    product.productTitle.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
     <div className="cards-wrapper">
       <div className="cards-top container">
         <h1>All sneakers</h1>
+        <div className="form-group">
+          <input type="text" onChange={handleSearch}   name="name" id="name" placeholder="Search..." />
+          <div className="icon"><LuSearch color="#E4E4E4" size={24} /></div>
+        </div>
       </div>
       <div className="cards container">
-        {products.map((product, index) => (
-          <ProductCard
-            key={index}
-            productTitle={product.productTitle}
-            productPrice={product.productPrice}
-            img={product.img}
-          />
-        ))}
+        
+      {filteredProducts.length === 0 ? (
+          <div className="text-not-found">Not Found</div>
+        ) : (
+          filteredProducts.map((product, index) => (
+            <ProductCard
+              key={index}
+              productTitle={product.productTitle}
+              productPrice={product.productPrice}
+              img={product.img}
+            />
+          ))
+        )}
       </div>
     </div>
   );
